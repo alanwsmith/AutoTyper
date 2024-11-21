c0c8968379d99f2e84a586082d94bfcacf031393
Alan Smith <commit@alanwsmith.com>
2024-11-21 11:16:43 -0500

    Initial Commit

---
+++ AutoTyper/AutoTyperApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoTyperApp.swift
+//  AutoTyper
+//
+//  Created by Alan W. Smith on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct AutoTyperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoTyperTests/AutoTyperTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutoTyperTests.swift
+//  AutoTyperTests
+//
+//  Created by Alan W. Smith on 11/21/24.
+//
+
+import Testing
+@testable import AutoTyper
+
+struct AutoTyperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
